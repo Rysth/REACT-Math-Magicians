e128fc24a64a9369e95f9a554060690b74274138
Rysth <johnpalacios.t@gmail.com>
2023-08-10 12:50:20 -0500

    Solved API_KEY safety.

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -3,20 +3,18 @@ import QuoteLoading from './QuoteLoading';
 import './Quote.css';
 
 const API = 'https://api.api-ninjas.com/v1/quotes';
-const API_KEY = 'ZHOU94s8oucCFyxrpI2JwA==M2wNPrWjlPP3IxK8';
 
 export default function Quote() {
   const [quote, setQuote] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-
   useEffect(() => {
     const fetchNinjaAPI = async () => {
       try {
         const response = await fetch(API, {
           method: 'GET',
           headers: {
-            'X-Api-Key': API_KEY,
+            'X-Api-Key': process.env.REACT_APP_API_KEY,
           },
         });
         const data = await response.json();
